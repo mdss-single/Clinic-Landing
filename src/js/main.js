d25e5fb2df5af848379d3f91c2f497d568b744ed
mdss@makexhtml.ru <mdss@makexhtml.ru>
2020-03-24 20:39:29 +0300

    Clinic landing page test work

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+(function($){
+	"use strict";
+
+	$('.js-menu').click(function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		showMenu();
+	});
+	$(document).click(function(e) {
+		if (!$(e.target).closest('.menu').length) {
+			hideMenu();
+		}
+	});
+
+	$('.js-reviews').flickity({
+		wrapAround: true,
+		prevNextButtons: false,
+	});
+
+	function showMenu() {
+		$('html').addClass('no-scroll');
+		$('body').prepend('<div class="menu__cover"></div>');
+		$('.menu').addClass('menu--active');
+		if (!$('.menu__cover').length) $('body').prepend('<div class="menu__cover"></div>');
+	}
+
+	function hideMenu() {
+		$('.menu').removeClass('menu--active');
+		if ($('.menu__cover').length) $('.menu__cover').remove();
+		if ($('html').hasClass('no-scroll')) $('html').removeClass('no-scroll');
+	}
+
+})(jQuery);
